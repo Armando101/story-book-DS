5ab9599d4024674c7a1a0a11e9370dc30bfb9ad0
Armando Rivera <rivera.armando997@gmail.com>
2024-01-04 08:57:33 -0600

    Add addon - view port

--- .storybook/preview.ts
+++ .storybook/preview.ts
@@ -1,4 +1,30 @@
 import type { Preview } from "@storybook/html";
+import { INITIAL_VIEWPORTS } from "@storybook/addon-viewport";
+import { MINIMAL_VIEWPORTS } from "@storybook/addon-viewport";
+
+const customViewPorts = {
+  kindleFireHD: {
+    name: "Kindle Fire HD",
+    styles: {
+      width: "533px",
+      height: "801px",
+    },
+  },
+  samsung: {
+    name: "samsung",
+    styles: {
+      width: "360px",
+      height: "540px",
+    },
+  },
+  tabletLite: {
+    name: "samsungHorizontal",
+    styles: {
+      width: "1024px",
+      height: "540px",
+    },
+  },
+};
 
 const preview: Preview = {
   parameters: {
@@ -8,6 +34,14 @@ const preview: Preview = {
         color: /(background|color)$/i,
         date: /Date$/i,
       },
+    },
+    viewport: {
+      viewports: {
+        ...customViewPorts,
+        ...MINIMAL_VIEWPORTS,
+        ...INITIAL_VIEWPORTS,
+      },
+      default: "someDefault",
     },
   },
 };
